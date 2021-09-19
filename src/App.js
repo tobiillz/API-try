adecc11ed820dd4918cc5b12be3ff80ba5251aab
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-09-19 23:49:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from 'firebase';
+import 'firebase/auth' ;
+import 'firebase/firestore' ;
+
+firebase.initializeApp({
+  var firebaseConfig = {
+    apiKey: "AIzaSyB9SxkV60yjzcChSWoijAvJVkv1rfq4TvM",
+    authDomain: "first-app-f8356.firebaseapp.com",
+    projectId: "first-app-f8356",
+    storageBucket: "first-app-f8356.appspot.com",
+    messagingSenderId: "169982882868",
+    appId: "1:169982882868:web:65b08647b3ad69b0e70661"
+  };
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
     </div>
   );
 }
